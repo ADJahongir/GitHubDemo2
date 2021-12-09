c32ad6b0eff8a1f9a75faa55a74b152a346680cb
jahongir <jaxobgir799@gmail.com>
2021-12-09 22:14:57 +0500

    Initial Commit

---
+++ GitHubDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubDemo2
+//
+//  Created by Jahongir on 09/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to SwitUI")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubDemo2/GitHubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemo2App.swift
+//  GitHubDemo2
+//
+//  Created by Jahongir on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
